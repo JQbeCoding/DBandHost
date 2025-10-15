162971290deb9ab57ff7f8516e8747793ca8dbaf
Ja'Quis <soxietymarketing@gmail.com>
2025-10-15 14:16:18 -0400

    Writing this file to a JSON

--- DB.c
+++ DB.c
@@ -29,7 +29,7 @@ int main(void)
 
     // Creating a File to write the final piece of information to
     FILE *maydayz_emp_inforamtion;
-    maydayz_emp_inforamtion = fopen("secrued_info.txt", "w");
+    maydayz_emp_inforamtion = fopen("secrued_info.json", "w");
     if (maydayz_emp_inforamtion == NULL)
     {
         perror("Error Opening text file for MayDayz Employees");
@@ -50,9 +50,11 @@ int main(void)
 
     struct Employee employee1 = createEmployee("Ja'Quis", "Avshia", "Franklin", "704-967-5408", "jaquis.franklin@maydayz.com", "6500 Monteith Drive",
                                                "M-3455663", "Avashia100604!!", "10/06/04", "06/01/24", "Admin", "CTO", 334844843, 01234567, 56.673445, "000-01-9989");
-    struct Employee employee2 = createEmployee("Lamar", "Jevon", "Henderson", "213-555-8192", "lamar.henderson@soxietytech.com", "8429 Sunset Blvd, Apt 12C", "HndrsonL001", "P@ssW0rd!93", "11/22/1993", "07/15/2022",
+    struct Employee employee2 = createEmployee("Lamar", "Jevon", "Henderson", "213-555-8192", "lamar.henderson@soxietytech.com", "8429 Sunset Blvd, Apt 12C",
+                                               "HndrsonL001", "P@ssW0rd!93", "11/22/1993", "07/15/2022",
                                                "Engineer", "Backend Developer", 547829103, 10293847, 50.75, "000-47-8392");
-    struct Employee employee3 = createEmployee("Lamar", "Jacob", "Green", "213-555-8192", "lamar.green@soxietytech.com", "9248 Sunrise Blvd, Apt 12A", "GreenL001", "P@ssW0rd!94", "11/22/2003", "07/15/2025",
+    struct Employee employee3 = createEmployee("Lamar", "Jacob", "Green", "213-555-8192", "lamar.green@soxietytech.com", "9248 Sunrise Blvd, Apt 12A", "GreenL001",
+                                               "P@ssW0rd!94", "11/22/2003", "07/15/2025",
                                                "Engineer", "Backend Developer", 547829103, 10293847, 50.75, "000-47-8392");
 
     *employees = employee1;
@@ -68,12 +70,6 @@ int main(void)
         free(employees);
         return 1;
     }
-    for (int i = 0; i < 3; i++)
-    {
-        size_t raw_password_len = strlen(employees[i].passcode);
-        memcpy(passwords + i * passcode_array_size, employees[i].passcode, passcode_array_size);
-        *(passwords + i * passcode_array_size + raw_password_len) = '\0';
-    }
     displayEmployees(employees, 10);
     searchEmployeeByName(employees);
     searchEmployeeByUsername(employees);
@@ -87,14 +83,26 @@ int main(void)
 
     displayEmployees(employees, 10);
     printf("\n");
-
+    fprintf(maydayz_emp_inforamtion, "{ \n");
+    fprintf(maydayz_emp_inforamtion, "  \"employees\": [\n");
     for (int i = 0; i < 3; i++)
     {
-        fprintf(maydayz_emp_inforamtion, "Name: %s %.1s %s\nDOB: %s\nAddress: %s\nRole: %s\nPosition: %s\nDate Hired: %s\n", employees[i].first_name, employees[i].middle_name, employees[i].last_name, employees[i].birthday,
-                employees[i].mailing_address, employees[i].role, employees[i].position, employees[i].hire_date);
-        fprintf(maydayz_emp_inforamtion, "\n");
+        fprintf(maydayz_emp_inforamtion,
+                "    {\n"
+                "      \"name\": \"%s %s %s\",\n"
+                "      \"dob\": \"%s\",\n"
+                "      \"address\": \"%s\",\n"
+                "      \"role\": \"%s\",\n"
+                "      \"position\": \"%s\",\n"
+                "      \"date_hired\": \"%s\"\n"
+                "    }%s\n",
+                employees[i].first_name, employees[i].middle_name, employees[i].last_name, employees[i].birthday,
+                employees[i].mailing_address, employees[i].role, employees[i].position, employees[i].hire_date,
+                // Added a ternary operator to check if the object is last
+                (i < 2) ? "," : "");
     }
-
+    fprintf(maydayz_emp_inforamtion, "  ]\n");
+    fprintf(maydayz_emp_inforamtion, "} ");
     printf("Successfully wrote to secured info file!\n");
 
     free(employees);
